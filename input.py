4f9b4b42c747e05a44dc42accb50d4a2ed65f63f
monihikari <69098341+monihikari@users.noreply.github.com>
2022-02-20 16:43:17 -0600

    Katas del 0 al 4

---
+++ input.py
@@ -0,0 +1,8 @@
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
